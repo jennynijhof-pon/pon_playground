62ee4ea6b40dd57313cb78070a57c801fcc3f0fb
jennynijhof-pon <jenny.nijhof@ponautomotive.com>
2026-09-22 10:44:57 +0200

    Delete dist directory

--- dist/src/utils/jiraConfig.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jiraConfig = void 0;
-exports.jiraConfig = {
-    baseUrl: 'https://ponautomotive.atlassian.net/',
-    email: process.env.JIRA_USERNAME,
-    apiToken: process.env.JIRA_API_TOKEN,
-    projectKey: process.env.JIRA_PROJECT_KEY,
-};
--- dist/src/utils/tdgConfig.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tdgConfig = void 0;
-exports.tdgConfig = {
-    baseUrl: 'https://prelive-central.omd.cloud.vwgroup.com/omd-server/nadin/main/testdatagen/testdata?lc=3406448',
-    email: process.env.JIRA_USERNAME,
-    password: process.env.TDG_PASSWORD
-};
